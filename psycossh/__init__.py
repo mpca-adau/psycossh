eb9ba8b7135936c760b5e8183557cd57fef086a1
Daniel Sullivan <daniel.sullivan@state.mn.us>
2020-12-03 12:22:56 -0600

    Cleanup and create_engine_cached

--- psycossh/__init__.py
+++ psycossh/__init__.py
@@ -27,62 +27,84 @@ class TunneledConnection(connection):
 
         Alternatively, various options can be set in advance on the class.
     """
+
     ssh_user = None
     ssh_pkey = None
     ssh_host = None
     ssh_port = None
     pg_host = None
+    pg_user = None
     pg_port = None
     pg_password = None
     _tunnel_connections = {}
 
-    def __init__(self, dsn, *args):
+    def __init__(self, dsn, *args, **kwargs):
         cparams = parse_dsn(dsn)
-        ssh_user = self.__class__.ssh_user or cparams['user']
-        ssh_pkey = self.__class__.ssh_pkey or cparams['password']
-        ssh_host = self.__class__.ssh_host or cparams['host']
+        ssh_user = self.__class__.ssh_user or cparams["user"]
+        ssh_pkey = self.__class__.ssh_pkey or cparams["password"]
+        ssh_host = self.__class__.ssh_host or cparams["host"]
         ssh_port = self.__class__.ssh_port or 22
-        pg_port = self.__class__.pg_port or cparams.get('port', None) or 5432
-        pg_server = self.__class__.pg_host or 'localhost'
+        pg_port = self.__class__.pg_port or cparams.get("port", None) or 5432
+        pg_user = self.__class__.pg_user or cparams["user"]
+        pg_server = self.__class__.pg_host or "localhost"
         pg_password = self.__class__.pg_password or None
         tun_spec = (ssh_host, ssh_port, ssh_user, ssh_pkey, pg_server, pg_port)
         if tun_spec not in self.__class__._tunnel_connections:
-            print('Creating Tunnel {}'.format(tun_spec))
+            print("Creating Tunnel {}".format(tun_spec))
             self.__class__._tunnel_connections[tun_spec] = open_tunnel(
-                    ssh_address_or_host=(ssh_host, ssh_port),
-                    ssh_username=ssh_user,
-                    ssh_pkey=ssh_pkey,
-                    remote_bind_address=(pg_server, pg_port),
-                    local_bind_address=('127.0.0.1',),
-                    compression=True,
-                    block_on_close=False
+                ssh_address_or_host=(ssh_host, ssh_port),
+                ssh_username=ssh_user,
+                ssh_pkey=ssh_pkey,
+                remote_bind_address=(pg_server, pg_port),
+                local_bind_address=("127.0.0.1",),
+                compression=True,
+                block_on_close=False,
             )
             self.__class__._tunnel_connections[tun_spec].start()
-        cparams['host'] = 'localhost'
-        cparams['port'] = self.__class__._tunnel_connections[tun_spec].local_bind_port
-        cparams['password'] = pg_password
+        cparams["host"] = "localhost"
+        cparams["port"] = self.__class__._tunnel_connections[tun_spec].local_bind_port
+        cparams["password"] = pg_password
+        cparams["user"] = pg_user
         start = timer()
         super(TunneledConnection, self).__init__(make_dsn(None, **cparams), *args)
-        print('{:0.2f}'.format(timer() - start))
+        print("{:0.2f}".format(timer() - start))
 
 
-_engine_cache = {}
+def create_tunneled_engine(
+    ssh_host,
+    ssh_user,
+    ssh_pkey,
+    database,
+    ssh_port=22,
+    pg_host="localhost",
+    pg_user=None,
+    pg_port=5432,
+    pg_password=None,
+    **kwargs,
+):
+
+    if pg_user is None:
+        pg_user = ssh_user
+
+    class _OurTunneledConnection(TunneledConnection):
+        pass
+
+    _OurTunneledConnection.ssh_host = ssh_host
+    _OurTunneledConnection.ssh_user = ssh_user
+    _OurTunneledConnection.ssh_pkey = ssh_pkey
+    _OurTunneledConnection.ssh_port = ssh_port
+    _OurTunneledConnection.pg_host = pg_host
+    _OurTunneledConnection.pg_user = pg_user
+    _OurTunneledConnection.pg_port = pg_port
+    _OurTunneledConnection.database = database
+    _OurTunneledConnection.pg_password = pg_password
+    return create_engine(
+        "postgresql://{user}@{host}/{database}".format(
+            user=pg_user, host=ssh_host, database=database
+        ),
+        connect_args={"connection_factory": _OurTunneledConnection},
+        **kwargs
+    )
 
 
-def create_engine_cached(*args, **kwargs):
-    ident = tuple(list(args) + list(to_tuple(kwargs)))
-    if ident not in _engine_cache:
-        print('--- creating engine')
-        _engine_cache[ident] = create_engine(*args, **kwargs)
-    else:
-        print('--- using cached engine')
-    return _engine_cache[ident]
-
-
-def to_tuple(obj):
-    if isinstance(obj, dict):
-        ret = tuple((k, to_tuple(v)) for k, v in obj.items())
-        # if len(ret) == 1:
-        #     return ret[0]
-        return ret
-    return obj
+__all__ = ["create_tunneled_engine"]
